b927c31efc4b1d88142e1a5e2ddba6ebe37d5c4e
Yasin Gündüz <yasin.guenduez1@endress.com>
2025-07-23 16:19:22 +0200

    add example project of serde

--- serde-crate/src/main.rs
+++ serde-crate/src/main.rs
@@ -1,3 +1,25 @@
+use std::path::Display;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Hans {
+    is_boss: bool,
+    company: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let hans = Hans {
+        is_boss: true,
+        company: String::from("EHDS"),
+    };
+
+    let json = serde_json::to_string(&hans).unwrap();
+
+    println!("{}", json);
+
+    let stringy = r#"{"is_boss":true,"company":"EHDS"}"#;
+    let other_hans: Hans = serde_json::from_str(stringy).unwrap();
+
+    println!("{:?}", other_hans);
 }
